2d987932692192e5f8c4aba44e6cb8a5bde4505b
ethicnology <ethicnology@pm.me>
2021-02-07 00:30:37 +0100

    Update comments

--- 8INF852TP.cpp
+++ 8INF852TP.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]){
 	if (argc != 4) {
 		cout << endl << "######## MISSING ARGUMENTS ########" << endl;
 		cout << "something.exe Iteration File Algorithm" << endl;
-		cout << "ex : 8INF852TP.exe 20 PROB40.txt vns" << endl;
+		cout << "ex : 8INF852TP.exe 20 PROB40.txt VNS" << endl;
 		return 0;
 	}
 	else {
