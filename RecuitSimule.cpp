a60d4cfd9f29b3758a91c72d6b7df34a6ddcd41e
Lleiro <leo.monteiro1@uqac.ca>
2021-02-07 22:01:44 -0500

    RecuitSimule.cpp

--- RecuitSimule.cpp
+++ RecuitSimule.cpp
@@ -5,16 +5,18 @@ void RecuitSimule(int iteration, SMSSDTProblem* leProb, int critereArret, int fc
 	clock_t	Start, Current;
 	SMSSDTSolution* Solution = new SMSSDTSolution(leProb->getN(), true);
 
-	double Tinitial = 500.0f;
+	double Tinitial = 50.0f;
 	double Tk = Tinitial;
 	int k = 1;
+	double condition = 1;
 
 	for (int j = 0; j < iteration; j++) {
 		Start = clock();
+		Current = clock();
 		SMSSDTSolution	BestSolution(leProb->getN());
 
 		do {
-			Permute(Solution, k);
+			Permute(Solution, 1);
 			Tools::Evaluer(leProb, *Solution);
 			int r = rand();
 
@@ -23,10 +25,18 @@ void RecuitSimule(int iteration, SMSSDTProblem* leProb, int critereArret, int fc
 				fctObjectif = BestSolution.getObj();
 			}
 
-			Tk = g(Tk);
+			if (k >= 20000 && k >= condition) {
+				cout << "je passe dedans " << k << endl;
+				cout << "température " << Tk << endl;
+				cout << "fctobjectif " << fctObjectif << endl;
+				cout << "============= " << endl;
+				Tk = g(Tk);
+				condition = k * 1.1;
+			}
 			k++;
+			
+			
 			Current = clock();
-			cout << "Valeur Fitness : " << Tk <<endl;
 		} while (Tk >= 0.0f && (((double(Current) - double(Start)) / CLOCKS_PER_SEC) < critereArret));
 
 		StopAndLog(Start, clock(), BestSolution, leProb->getNomFichier());
@@ -44,7 +54,7 @@ double p(double Tk, double solutionCourante, double solutionVoisine)
 
 double g(double Tk)
 {
-	double alpha = 0.975;
+	double alpha = 0.90;
 	return Tk * alpha;
 }
 
